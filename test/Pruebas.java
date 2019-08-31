5046639f5bc9c0b34e11ac1d14f86b438ca5a55b
GabrielaMachado <gabimachadop@hotmail.com>
2019-08-31 03:12:50 -0500

    Pruebas 1

--- test/Pruebas.java
+++ test/Pruebas.java
@@ -45,18 +45,31 @@ public class Pruebas {
     // public void hello() {}
     @Test
     public void AutomataLiteralBooleano() {
-//        Automata aur = new Automata();
-//
-//  //      Lexema lexe = aur.esLiteralBooleana(cadena, 0);
-//
-//        int esperado = 1, resultado;
-//
-//        if (lexe != null) {
-//            resultado = 1;
-//        } else {
-//            resultado = 0;
-//        }
-//
-//        assertEquals(esperado, resultado);
+        Automata prueba = new Automata();
+        //  Character [] palabra;
+        //    Lexema lexe = ;
+        int esperado = 1, resultado;
+
+        if (prueba.esLiteralBooleana(transformar("yalse"), 0) != null) {
+            resultado = 1;
+        } else {
+            resultado = 0;
+        }
+        assertEquals(esperado, resultado);
+
+        //assertTrue(prueba.esLiteralBooleana(transformar("false"), 0) != null);
+    }
+
+    public Character[] transformar(String texto) {
+        String res = " " + texto + " ";
+        int longitud;
+        Character ch[];
+        res = res.toLowerCase();
+        longitud = res.length();
+        ch = new Character[longitud];
+        for (int i = 0; i < longitud; i++) {
+            ch[i] = res.charAt(i);
+        }
+        return ch;
     }
 }
